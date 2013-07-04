08e9db61194d907c8cd0a0fbe3a33ece67f77f58
Vincent Fiorentini <vfiorentini@yammer-inc.com>
2013-07-03 23:34:26 -0700

    Completed tagging feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(
+      author_name: params[:comment][:author_name],
+      body: params[:comment][:body],
+      article_id: params[:article_id])
+    @comment.save
+    redirect_to article_path(@comment.article)
+  end
+end
